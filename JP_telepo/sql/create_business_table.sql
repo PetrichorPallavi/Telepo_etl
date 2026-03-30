2ad02987063a0e2dcb58c2bb844df5f9016d6d1f
PetrichorPallavi <pallavishiwankar727@gmail.com>
2026-03-30 17:43:02 +0900

    Update create_business_table.sql

--- JP_telepo/sql/create_business_table.sql
+++ JP_telepo/sql/create_business_table.sql
@@ -1,49 +1,23 @@
--- 既存テーブルを削除（存在する場合）
+-- SQL script to create a business search table for Tokyo / 東京の事業検索テーブルを作成するSQLスクリプト
+
+-- Drop existing table if exists / 既存テーブルがあれば削除
 DROP TABLE IF EXISTS tokyo_business_search;
 
--- ビジネス検索用テーブルを作成
+-- Create new table from planet_osm_point / planet_osm_point から新規テーブル作成
 CREATE TABLE tokyo_business_search AS
-
--- -----------------------------
--- ノード（ポイント）データの店舗・施設
--- -----------------------------
 SELECT
-    name,                         -- 名称
-    amenity,                      -- 施設カテゴリ
-    shop,                         -- 店舗種別
-    tourism,                      -- 観光関連
-    tags->'phone' AS phone,       -- 電話番号
-    tags->'website' AS website,   -- ウェブサイト
-    tags->'addr:housenumber' AS housenumber, -- 番地
-    tags->'addr:street' AS street,            -- 通り名
-    way                           -- 位置情報（ポイント）
+    name,                       -- Business name / 事業名
+    amenity,                     -- Amenity type / 設備タイプ
+    shop,                        -- Shop type / 店舗タイプ
+    tourism,                     -- Tourism type / 観光施設タイプ
+    tags->'phone' AS phone,      -- Phone number / 電話番号
+    tags->'website' AS website,  -- Website URL / ウェブサイト
+    tags->'addr:housenumber' AS housenumber,  -- House number / 番地
+    tags->'addr:street' AS street,            -- Street name / 通り名
+    way                          -- Geometry / 幾何情報
 FROM planet_osm_point
-WHERE name IS NOT NULL
+WHERE name IS NOT NULL           -- Must have a name / 名前が必須
 AND (
-    amenity IS NOT NULL
-    OR shop IS NOT NULL
-    OR tourism IS NOT NULL
-)
-
-UNION ALL
-
--- -----------------------------
--- 建物（ポリゴン）データの店舗・施設
--- -----------------------------
-SELECT
-    name,                         -- 名称
-    amenity,                      -- 施設カテゴリ
-    shop,                         -- 店舗種別
-    tourism,                      -- 観光関連
-    tags->'phone' AS phone,       -- 電話番号
-    tags->'website' AS website,   -- ウェブサイト
-    tags->'addr:housenumber' AS housenumber, -- 番地
-    tags->'addr:street' AS street,            -- 通り名
-    ST_Centroid(way) AS way       -- ポリゴンの中心点を取得
-FROM planet_osm_polygon
-WHERE name IS NOT NULL
-AND (
-    amenity IS NOT NULL
-    OR shop IS NOT NULL
+    amenity IS NOT NULL           -- Has amenity OR tourism type / 設備タイプまたは観光施設タイプがある
     OR tourism IS NOT NULL
 );
